aee7bdec622d7cca5b1c8517358c74e6a79597e6
moshemoses <kartenm@gmail.com>
2021-02-14 23:36:34 -0500

    adding emailer and main function

---
+++ emailer.py
@@ -0,0 +1,26 @@
+import smtplib, ssl
+import json
+config = json.load(open('config.json', 'r'))
+
+def emailer(text):
+    smtp_server = "smtp.gmail.com"
+    port = 587  # For starttls
+    sender_email = config["username"]
+    password = config["password"]
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    # Try to log in to server and send email
+    try:
+        server = smtplib.SMTP(smtp_server,port)
+        server.ehlo() # Can be omitted
+        server.starttls(context=context) # Secure the connection
+        server.ehlo() # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail( config["username"], config["recipient"],text)
+    except Exception as e:
+        # Print any error messages to stdout
+        print(e)
+    finally:
+        server.quit() 
--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options
+import datetime
+
+import emailer
 chrome_options = Options()
 #chrome_options.add_argument("--disable-extensions")
 #chrome_options.add_argument("--disable-gpu")
@@ -7,31 +10,49 @@ chrome_options = Options()
 
 #headless option
 #chrome_options.add_argument("--headless")
+logFile = open('vaxBotLog.txt', 'a')
+logFile.write('\n open file: {} \n'.format(datetime.datetime.now()))
+try:
+    # chrome_options.headless = True # also works
+    driver = webdriver.Chrome(options=chrome_options)
+    url = "https://www.cvs.com/immunizations/covid-19-vaccine"
+    driver.get(url)
+
+    assert "COVID Vaccine (COVID-19 Immunization Updates)" in driver.title
+
+    driver.find_element_by_link_text('New Jersey').click()
+
+    cities = driver.find_elements_by_class_name("city")
+    vaxStatus = driver.find_elements_by_class_name("status")
 
 
-# chrome_options.headless = True # also works
-driver = webdriver.Chrome(options=chrome_options)
-url = "https://www.cvs.com/immunizations/covid-19-vaccine"
-driver.get(url)
+    citiesLength = len(cities)
+    print('cities:', citiesLength)
+    vaxStatusLength = len(vaxStatus)
+    print('status:', vaxStatusLength)
+    emailText = 'CVS Appointments available at: \n'
+    emailUpdates = []
+    isTriggered = False
+    if citiesLength == vaxStatusLength:
+        print('same number of statuses as cities - continue to check')
+    else:
+        logFile.write('Number of cities do not match the number of statuses \n')
+    for row in range(0, citiesLength):
+        vaxStatText = vaxStatus[row].text
+        cityText = cities[row].text
+        if vaxStatText != 'Fully Booked':
+            emailUpdates.push('\t {} -- {} \n'.format(cityText, vaxStatText))
+        logFile.write('\t {} -- {} \n'.format(cityText, vaxStatText))
 
-assert "COVID Vaccine (COVID-19 Immunization Updates)" in driver.title
+    if isTriggered: 
+        for update in emailUpdates:
+            emailText += update
+        emailer.emailer(emailText)
+        logFile.write('\t Appointment found! \n {} \n'.format(emailText))
 
-driver.find_element_by_link_text('New Jersey').click()
-
-cities = driver.find_elements_by_class_name("city")
-vaxStatus = driver.find_elements_by_class_name("status")
-
-
-citiesLength = len(cities)
-print('cities:', citiesLength)
-vaxStatusLength = len(vaxStatus)
-print('status:', vaxStatusLength)
-
-if citiesLength == vaxStatusLength:
-    print('same number of statuses as cities - continue to check')
-else:
-    print('Number of cities do not match the number of statuses')
-for row in range(0, citiesLength):
-    print(cities[row].text + '--' + vaxStatus[row].text)
-
-driver.quit()
+except Exception as e:
+    logFile.write('Error: {} - {}\n'.format(e, datetime.datetime.now()))
+finally:
+    driver.quit()
+    logFile.write('Close File: {} \n'.format(datetime.datetime.now()))
+    logFile.close()
